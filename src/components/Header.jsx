60716ae7b4c5a18f76cfe076f7e459a690f82a11
sreeharisreehari <kisreehari52@gmail.com>
2024-07-01 17:16:49 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+
+<div className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div className="container d-flex justify-content-center">
+    <h2 className="title mx-auto">Mr Filmer</h2>
+  </div>
+</div>
+
+
+        
+    </div>
+  )
+}
+
+export default Header
